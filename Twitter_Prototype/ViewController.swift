e7df58f87fb4ccb3872790af413dfdf3b14736c6
Modest J <modestjuarez@gmail.com>
2020-10-09 02:42:51 -0700

    Initial Commit

---
+++ Twitter_Prototype/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Twitter_Prototype
+//
+//  Created by Modest Juarez on 10/9/20.
+//  Copyright © 2020 Modest Juarez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
